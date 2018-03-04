2a62610721a77c5996f3b5962db4ff7a00f6e2ea
nick <nickantonovme@hotmail.com>
2018-03-04 19:18:15 +0200

    knknkklnnklnkl
    lkjjknnbbbhjbhjb

--- Regina/manipulator_js_bitronics_new/manipulator_js_bitronics_new.ino
+++ Regina/manipulator_js_bitronics_new/manipulator_js_bitronics_new.ino
@@ -8,9 +8,9 @@
 LiquidCrystal_I2C lcd(0x27, 16, 2);
 
 // чувствительность срабатывания
-double n0 = 1.571; 
+double n0 = 1.871; 
 double n1 = 1.671; 
-double n2 = 1.823;
+double n2 = 0.823;
 int reg = 1;
 int r = 4;
 int g = 8;
